266ba564a6d58bb7ed95157cc109b83ce1bf7a12
Jerry <jl4533@drexel.edu>
2024-08-20 06:14:13 -0400

    eda

---
+++ code/EDA.R
@@ -0,0 +1,114 @@
+library(ggplot2)
+library(dplyr)
+library(corrplot)
+
+#load the dataset
+fifa_data <- read.csv("../data/FIFA22_official_data.csv") 
+
+# get information about dataset
+head(fifa_data)
+str(fifa_data)
+summary(fifa_data)
+
+# find the shape of dataset
+shape <- dim(fifa_data)
+shape
+
+## > shape
+## [1] 16710    65
+
+# find the missing value
+sum(is.na(fifa_data))
+
+# find the missing value in each column
+missing_values_per_column <- colSums(is.na(fifa_data))
+missing_values_per_column
+
+# I find I get 15818 data have missing value in marking column, which is abnormal, 
+# so I check the data and found this is data for legend players. 
+# Since this project I only want to focus on current player, I decided to remove these players.
+fifa_data_clean <- fifa_data[is.na(fifa_data$Marking), ]
+dim(fifa_data_clean)
+
+# After I remove all the legend player, almost all the missing value problem have solved.
+# I still have 16 players have missing value in Jersy.Number, so I decided to use mode of jersy number to replace them.
+missing_values_per_column_clean <- colSums(is.na(fifa_data_clean))
+missing_values_per_column_clean
+
+# use mode to replace missing value in jersey number
+jersey_mode <- as.numeric(names(sort(table(fifa_data_clean$Jersey.Number), decreasing = TRUE)[1]))
+fifa_data_clean$Jersey.Number[is.na(fifa_data_clean$Jersey.Number)] <- jersey_mode
+
+
+missing_values_per_column_clean <- colSums(is.na(fifa_data_clean))
+missing_values_per_column_clean
+
+# Since current player don't have marking, I decide to remove this column
+fifa_data_clean <- fifa_data_clean %>% select(-Marking)
+
+# check if all the missing value have been processed
+sum(is.na(fifa_data_clean))
+
+# get overall view of each column
+summary(fifa_data_clean)
+
+# Find distribution of overall player rating distribution
+ggplot(fifa_data_clean, aes(x=Overall)) + 
+  geom_histogram(binwidth=1, fill="lightblue", color="black") +
+  labs(title="Overall Rating Distribution", x="Overall Rating", y="Frequency")
+
+# Analyse player age distribution
+ggplot(fifa_data_clean, aes(x=Age)) +
+  geom_histogram(binwidth=1, fill="lightblue", color="black") +
+  labs(title="Age Distribution", x="Age", y="Frequency")
+
+# Analyse preferred foot distribution
+ggplot(fifa_data_clean, aes(x=Preferred.Foot)) +
+  geom_bar(fill="darkgreen") +
+  labs(title="Preferred Foot Distribution", x="Preferred Foot", y="Frequency")
+
+# find club that have top 10 overall player rating
+top_10_clubs <- fifa_data_clean %>%
+  group_by(Club) %>%
+  summarize(Average_Overall = mean(Overall, na.rm = TRUE)) %>%
+  arrange(desc(Average_Overall)) %>%
+  slice_head(n = 10)
+
+# find the top 10 clubs 
+top_10_clubs_data <- fifa_data_clean %>%
+  filter(Club %in% top_10_clubs$Club)
+
+# plot the box plot
+ggplot(top_10_clubs_data, aes(x = reorder(Club, Overall, FUN = median), y = Overall)) +
+  geom_boxplot(fill = "lightblue", color = NA) +
+  stat_summary(fun = mean, geom = "point", shape = 21, size = 1, fill = "blue") +
+  stat_summary(fun = mean, geom = "text", aes(label = round(..y..)), vjust = -0.5, color = "black", size = 3) + # use integer to show average rating
+  coord_flip() +
+  labs(title = "Top 10 Club Overall Rating Distribution", x = "Club", y = "Overall Rating") +
+  theme_minimal()
+
+
+# find the top 10 countries that by calculating the top 50 player's average score
+top_10_countries <- fifa_data_clean %>%
+  group_by(Nationality) %>%
+  arrange(desc(Overall)) %>%
+  slice_max(Overall, n = 50, with_ties = FALSE) %>%
+  summarize(Average_Overall = mean(Overall, na.rm = TRUE)) %>%
+  arrange(desc(Average_Overall)) %>%
+  top_n(10, wt = Average_Overall)
+
+# find the top 10 countries from dataset
+top_10_countries_data <- fifa_data_clean %>%
+  filter(Nationality %in% top_10_countries$Nationality)
+
+
+
+ggplot(top_10_countries_data, aes(x = reorder(Nationality, Overall, FUN = median), y = Overall)) +
+  geom_boxplot(fill = "lightblue", color = NA) +
+  stat_summary(fun = mean, geom = "point", shape = 21, size = 1, fill = "blue") +
+  stat_summary(fun = mean, geom = "text", aes(label = round(..y..)), vjust = -0.5, color = "black", size = 3) +  # use integer to show average rating
+  coord_flip() +
+  labs(title = "Overall Score Distribution of Top 50 Players from the Top 10 Countries", x = "Nationality", y = "Overall Score") +
+  theme_minimal()
+
+
---
+++ proposal.md
@@ -0,0 +1,37 @@
+## Proposal for Multivariate Data Analysis Project
+
+#### Dataset Selection and Relevance
+
+For this project, I have selected the **FIFA 22** dataset, which contains extensive data on professional soccer players. The dataset includes 65 features, such as player attributes, ratings, and physical characteristics, with 16,710 samples. As a soccer enthusiast, I find this dataset particularly interesting because it provides a rich source of data that can be analyzed to uncover patterns and insights within the world of soccer. The diverse features in this dataset allow for a comprehensive exploration of how various attributes correlate with player performance, market value, and potential growth.
+
+#### Source of Dataset
+
+My dataset is download from [Kaggle&#39;s FIFA Player Stats Database](https://www.kaggle.com/datasets/bryanb/fifa-player-stats-database?resource=download).
+
+#### Description of Variables
+
+The FIFA 22 dataset contains a comprehensive set of 65 features. Due to space limitations, I will highlight a few key variables as examples:
+
+* **Overall Rating** : A numerical score representing the player's overall performance level.
+* **Potential** : The projected future rating of the player, indicating their potential for improvement.
+* **Position** : The specific role or position that the player occupies on the field (e.g., Forward, Midfielder, Defender).
+* **Height and Weight** : Physical measurements of the players that could impact their style of play and effectiveness.
+* **Skill Moves and Weak Foot** : Technical attributes that measure the player’s dribbling ability and proficiency with their non-dominant foot.
+* **International Reputation** : A measure of how well-known the player is on the international stage, potentially impacting their market value.
+
+These variables help us understand how different qualities affect a player's success in soccer. The variety in the dataset allows us to look closely at what influences both individual and team performance.
+
+#### Initial Hypotheses
+
+Based on the variables provided in the dataset, the following hypotheses will guide the analysis:
+
+- **Hypothesis 1** : Physical attributes such as height and weight are more influential for players in defensive positions than those in offensive positions, as physicality is often crucial for defensive roles.
+- **Hypothesis 2** : Goalkeepers with higher reflex ratings tend to have better overall ratings, indicating that reflexes are a crucial attribute for goalkeeping success.
+- **Hypothesis 3** : Players from more prestigious clubs (based on club reputation) tend to have higher market values, suggesting that club prestige influences player valuation.
+
+#### Expected Outcomes
+
+Through this analysis, I want to confirm or reject these hypotheses by exploring the relationships between key variables. The findings will highlight factors that contribute to a player's success and offer insights for optimizing player development and scouting strategies in professional soccer.
+
+#### Code
+You can access the code for this project on my GitHub repository. It includes all the scripts used for data analysis, from data cleaning to the final models. You can find it [here](https://github.com/JerryLi4533/INFO432_Final_Project.git).
